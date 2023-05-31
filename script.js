876a4bdb3a27a74f351f2541e4dcd8fcfa431418
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-31 17:28:11 +0200

    Nav bar managed

---
+++ script.js
@@ -0,0 +1,28 @@
+const toggler = document.querySelector('button');
+const navBar = document.querySelector(".navBar");
+const menu = document.querySelector(".mobile-toggle");
+const buttons = document.querySelectorAll(".nav-btn");
+
+toggler.addEventListener('click', () => {
+    if (toggler.classList.contains("fa-bars")) {
+        toggler.classList.add("fa-xmark");
+        toggler.classList.remove("fa-bars");
+        menu.classList.remove("invisible");
+    } else {
+        toggler.classList.add("fa-bars");
+        toggler.classList.remove("fa-xmark");
+        menu.classList.add("invisible");
+    }
+});
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        buttons.forEach(b => {
+            b.classList.remove("text--state-selected");
+        })
+        btn.classList.add("text--state-selected");
+        toggler.classList.add("fa-bars");
+        toggler.classList.remove("fa-xmark");
+        menu.classList.add("invisible");
+    })
+});
